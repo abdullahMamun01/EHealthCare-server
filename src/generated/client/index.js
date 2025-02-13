f3768040b73e82b86821e1bdc978a2239928898b
Abdullah Mamun <abdullah.mamun.0110@gmail.com>
2025-02-13 15:38:19 +0600

    generate prisma type

--- src/generated/client/index.js
+++ src/generated/client/index.js
@@ -368,7 +368,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../.env",
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
