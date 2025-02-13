f94cef130d94d1a9a874d0ccd463171297c71b03
Abdullah Mamun <abdullah.mamun.0110@gmail.com>
2025-02-13 15:08:59 +0600

    generate prisma client

--- prisma/generated/client/edge.js
+++ prisma/generated/client/edge.js
@@ -367,7 +367,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../.env",
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
