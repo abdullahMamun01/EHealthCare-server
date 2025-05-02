932dfc559362f12101f972201082fd9df68d9428
Abdullah Mamun <abdullah.mamun.0110@gmail.com>
2025-05-02 23:39:39 +0600

    feat(auth): enhance signin method to set authentication cookie on successful login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, UsePipes } from '@nestjs/common';
+import { Controller, Post, Body, UsePipes, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import {
   LoginDto,
@@ -10,6 +10,8 @@ import { ZodValidationPipe } from 'src/others/zodValidationPipe';
 import { Public } from './metadata';
 
 import { AuthSwagger } from './swagger/auth.swagger';
+import { Response } from 'express';
+
 
 @Controller('auth')
 export class AuthController {
@@ -27,7 +29,15 @@ export class AuthController {
   @Public()
   @Post('signin')
   @UsePipes(new ZodValidationPipe(loginSchema))
-  async signin(@Body() loginDto: LoginDto) {
-    return this.authService.signin(loginDto);
+  async signin( @Res({ passthrough: true }) response: Response,@Body() loginDto: LoginDto) {
+    const user = await this.authService.signin(loginDto);
+    if (user) {
+      response.cookie('token', user.accessToken, {
+        httpOnly: true, // Prevents JavaScript access to the cookie
+        secure: true, // Set to true in production
+        maxAge: 24 * 3600 * 1000, // 1 day expiration
+      });
+    }
+    return user
   }
 }
